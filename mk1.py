165753dc70329e1faff373697b3d8645776e912f
jashtailor <59695729+jashtailor@users.noreply.github.com>
2021-08-30 14:54:55 +0530

    Update mk1.py

--- mk1.py
+++ mk1.py
@@ -11,9 +11,19 @@ import plotly.figure_factory as ff
 from datetime import date
 import streamlit as st
 
-# Use the full page instead of a narrow central column
-st.beta_set_page_config(layout="wide")
+def my_widget(key):
+    st.subheader('Hello there!')
+    clicked = st.button("Click me " + key)
 
-# Space out the maps so the first one is 2x the size of the other three
-c1, c2, c3, c4 = st.beta_columns((2, 1, 1, 1))
+# This works in the main area
+clicked = my_widget("first")
+
+# And within an expander
+my_expander = st.beta_expander("Expand", expanded=True)
+with my_expander:
+    clicked = my_widget("second")
+
+# AND in st.sidebar!
+with st.sidebar:
+    clicked = my_widget("third")
 
