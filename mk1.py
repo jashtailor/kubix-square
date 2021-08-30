bae771509f5adb4ac1e30f0f030a6ddc3fd0c9dc
jashtailor <59695729+jashtailor@users.noreply.github.com>
2021-08-30 14:52:15 +0530

    Update mk1.py

--- mk1.py
+++ mk1.py
@@ -10,3 +10,10 @@ import plotly.graph_objects as go
 import plotly.figure_factory as ff
 from datetime import date
 import streamlit as st
+
+# Use the full page instead of a narrow central column
+st.beta_set_page_config(layout="wide")
+
+# Space out the maps so the first one is 2x the size of the other three
+c1, c2, c3, c4 = st.beta_columns((2, 1, 1, 1))
+
