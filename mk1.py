cbab3bf2ff538b59966904fd02c68eff3e5119c6
jashtailor <59695729+jashtailor@users.noreply.github.com>
2021-08-30 17:40:40 +0530

    Update mk1.py

--- mk1.py
+++ mk1.py
@@ -79,21 +79,9 @@ all_articles = newsapi.get_everything(q='Bitcoin',
 st.write("""
 # Malkovich """)
 
-st.text('This will appear first')
-# Appends some text to the app.
+dataframe = pd.DataFrame(
+    np.random.randn(10, 20),
+    columns=('col %d' % i for i in range(20)))
 
-my_slot1 = st.empty()
-# Appends an empty slot to the app. We'll use this later.
-
-my_slot2 = st.empty()
-# Appends another empty slot.
-
-st.text('This will appear last')
-# Appends some more text to the app.
-
-my_slot1.text('This will appear second')
-# Replaces the first empty slot with a text string.
-
-my_slot2.line_chart(np.random.randn(20, 2))
-# Replaces the second empty slot with a chart.
+st.dataframe(dataframe.style.highlight_max(axis=0))
 
