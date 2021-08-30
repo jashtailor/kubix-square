578b61e11b7aebfe780be6cc6f90fb04518dbe0f
jashtailor <59695729+jashtailor@users.noreply.github.com>
2021-08-30 15:37:22 +0530

    Update mk1.py

--- mk1.py
+++ mk1.py
@@ -16,9 +16,6 @@ import simplejson as json
 import streamlit as st
 
 
-
-# c1, c2, c3, c4 = st.beta_columns((2, 1, 1, 1))
-
 # COINBASE API
 # Before we take data from Twitter we need to know the top 10 cryptocurrencies based on market capitalization for which we use the Coinbase API
 url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest'
@@ -50,6 +47,7 @@ for i in range(0,10):
     symbol.append(data['data'][i]['symbol'])
     search_words.append(data['data'][i]['name'])
 
+col1,col2 = st.beta_columns(2)        
 option = st.selectbox(
      'Please select your preferred cryptocurrency',
      ('None', 'All', search_words[0], search_words[1], search_words[2], search_words[3], search_words[4], search_words[5], search_words[6], search_words[7], search_words[8], search_words[9]))
@@ -58,27 +56,7 @@ if option == 'All':
 elif option == search_words[0]:
   sentiment_analysis()
  
-st.set_page_config(layout="wide")
-    
-def my_widget(key):
-    st.subheader('Hello there!')    
-   
 
-# This works in the main area
-clicked = my_widget("first")
-
-
-
-st.write(search_words)
-    
-col1, col2 = st.beta_columns(2)
-col1.write('I am base col1!')
-col2.write('I am base col2!')
-
-# And within an expander
-my_expander = st.beta_expander("Expand", expanded=True)
-with my_expander:
-    clicked = my_widget("second")
 
 
 
