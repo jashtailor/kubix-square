1ad771925075a4d397ee64ccfd36a17e413a133b
jashtailor <59695729+jashtailor@users.noreply.github.com>
2021-08-30 17:55:17 +0530

    Update mk1.py

--- mk1.py
+++ mk1.py
@@ -81,10 +81,23 @@ st.write("""
 
 
 
-st.title("Let's create a table!")
-for i in range(1, 10):
-    cols = st.beta_columns(4)
-    cols[0].write('Name')
-    cols[1].write('Symbol')
-    cols[2].write(f'{i * i * i}')
-    cols[3].write('x' * i)
+# Randomly fill a dataframe and cache it
+@st.cache(allow_output_mutation=True)
+def get_dataframe():
+    return pd.DataFrame(
+        np.random.randn(50, 20),
+        columns=('col %d' % i for i in range(20)))
+
+
+df = get_dataframe()
+
+# Create row, column, and value inputs
+row = st.number_input('row', max_value=df.shape[0])
+col = st.number_input('column', max_value=df.shape[1])
+value = st.number_input('value')
+
+# Change the entry at (row, col) to the given value
+df.values[row][col] = value
+
+# And display the result!
+st.dataframe(df)
